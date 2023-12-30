387e5e493bc2b9ed3cd1a21bcc40b83a0284178b
iwang19 <ivanpratama2008@gmail.com>
2023-12-30 08:57:12 +0700

    Menambah Hitung Luas Persegi Panjang

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 #Hitung Luas Segitiga sederhana
+
 def luas_segitiga():
     a = int(input("masukkan alas segitiga: "))
     t = int(input("Masukkan tinggi segitiga: "))
@@ -6,3 +7,12 @@ def luas_segitiga():
     print("Luas segitiga adalah: ", luas)
 
 luas_segitiga()
+
+#Hitung Luas Persegi Panjang
+def luas_persegi_panjang():
+    p = int(input("Masukkan panjang persegi: "))
+    l = int(input("Masukkan lebar persegi: "))
+    luas = p * l\
+    print("Luas persegi adalah: ", luas)
+
+luas_persegi_panjang()
