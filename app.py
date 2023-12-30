2a92bec34246f6a796583639e74b2f6cd4888daf
iwang19 <ivanpratama2008@gmail.com>
2023-12-30 09:41:05 +0700

    Kolaborasi Pertama

--- app.py
+++ app.py
@@ -13,6 +13,16 @@ def luas_persegi_panjang():
     p = int(input("Masukkan panjang persegi: "))
     l = int(input("Masukkan lebar persegi: "))
     luas = p * l\
+    
     print("Luas persegi adalah: ", luas)
 
 luas_persegi_panjang()
+
+#Menghitung Teorema Pytagoras
+def main():
+    a = float(input("Masukkan nilai a: "))
+    b = float(input("Masukkan nilai b: "))
+    c = (a**2 + b**2)**0.5
+    print("Nilai c adalah: ", c)
+
+main()
